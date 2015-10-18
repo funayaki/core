a426ea62419e3f503bce9fbd1cfee1a9dd75ef3e
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-10-18 18:02:36 +0200

    Get the core unit tests to run

---
+++ src/Controller/HookableComponentTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Croogo\Core\Controller;
+
+use Cake\Event\Event;
+
+trait HookableComponentTrait
+{
+
+	public function dispatchBeforeInitialize()
+	{
+		$this->eventManager()->dispatch(new Event('Controller.beforeInitialize', $this));
+	}
+
+	public function loadHookableComponent($name, array $config)
+	{
+		list(, $prop) = pluginSplit($name);
+		list(, $modelProp) = pluginSplit($this->modelClass);
+
+		$component = $this->components()->load($name, $config);
+		if ($prop !== $modelProp) {
+			$this->{$prop} = $component;
+		}
+
+		return $component;
+	}
+}
